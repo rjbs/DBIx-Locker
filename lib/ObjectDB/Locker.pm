ed9e7f2e7520f0a258739ed523902815c2b7fc2d
Ricardo SIGNES <rjbs@cpan.org>
2008-02-13 19:30:23 +0000

    husk
    git-svn-id: svn+ssh://svn.pobox.com/svn/ObjectDB-Locker/trunk@36267 1bcdbe44-fcfd-0310-b51b-975661d93aa0

---
+++ lib/ObjectDB/Locker.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use 5.006;
+
+package ObjectDB::Locker;
+our $VERSION = '0.001';
+
+=head1 NAME
+
+ObjectDB::Locker - locks for ObjectDB that might not be totally insane
+
+=cut
+
+sub new {
+  my ($class) = @_;
+
+}
+
+=head1 COPYRIGHT AND LICENSE
+
+Proprietary.  Copyright 2008 IC Group
+
+All rights reserved.
+
+=cut
+
+1;
