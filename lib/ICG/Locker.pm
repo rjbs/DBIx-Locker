0d52c41a12fc52a691d787511831626449697aff
Ricardo SIGNES <rjbs@cpan.org>
2008-02-13 20:43:19 +0000

    first pass
    git-svn-id: svn+ssh://svn.pobox.com/svn/ICG-Locker/trunk@36275 1bcdbe44-fcfd-0310-b51b-975661d93aa0

--- lib/ICG/Locker.pm
+++ lib/ICG/Locker.pm
@@ -1,19 +1,114 @@
 use strict;
 use warnings;
-use 5.006;
+use 5.008;
 
-package ObjectDB::Locker;
+package ICG::Locker;
 our $VERSION = '0.001';
+
+use ICG::Handy ();
+use JSON::XS ();
+use Data::GUID ();
 
 =head1 NAME
 
-ObjectDB::Locker - locks for ObjectDB that might not be totally insane
+ICG::Locker - locks for db resources that might not be totally insane
+
+=head1 METHODS
+
+=head2 new
+
+  my $locker = ICG::Locker->new(\%arg);
+
+This returns a new locker. 
+
+Valid arguments are:
+
+  dbh      - a database handle to use for locking
+  dbi_args - an arrayref of args to pass to DBI->connect to reconnect to db
+  table    - the table for locks
 
 =cut
 
 sub new {
-  my ($class) = @_;
+  my ($class, $arg) = @_;
 
+  my $guts = {
+    dbh      => $arg->{dbh},
+    dbi_args => ($arg->{dbi_args} || Carp::confess 'arg dbi_args is required'),
+    table    => ($arg->{table}    || Carp::confess 'arg table is required'),
+  };
+
+  return bless $guts => $class;
+}
+
+=head2 dbh
+
+This method returns the locker's dbh.
+
+=cut
+
+sub dbh {
+  my ($self) = @_;
+  return $self->{dbh} if $self->{dbh} and eval { $self->{dbh}->ping };
+
+  X::Unavailable->throw("couldn't connect to database: $DBI::errstr")
+    unless my $dbh = DBI->connect(@{ $self->{dbi_args} });
+
+  return $self->{dbh} = $dbh;
+}
+
+=head2 lock
+
+  my $lock = $locker->lock($identifier, \%arg);
+
+This method attempts to return a new ICG::Locker::Lock.
+
+=cut
+
+my $JSON;
+BEGIN { $JSON = JSON::XS->new->canonical(1)->space_after(1); }
+
+sub lock {
+  my ($self, $ident, $arg) = @_;
+  $arg ||= {};
+
+  X::BadValue->throw('must provide a lockstring')
+    unless defined $ident and length $ident;
+
+  my $expires = $arg->{expires} ||= 3600;
+
+  X::BadValue->throw('expires must be a positive integer')
+    unless $expires > 0 and $expires == int $expires;
+
+  $expires = time + $expires;
+
+  my $locked_by = {
+    guid => Data::GUID->new->as_string,
+    pid  => $$,
+    for  => $arg->{for},
+  };
+
+  my $table = $self->table;
+  my $dbh   = $self->dbh;
+  my $rows  = $dbh->do(
+    "INSERT INTO $table (lockstring, expires, locked_by)
+    VALUES (?, ?, ?, ?)",
+    undef,
+    $ident,
+    ICG::Handy::then14(localtime($expires)),
+    $JSON->encode_json($locked_by),
+  );
+ 
+  X::Unavailable->throw('could not lock resource') unless $rows == 1;
+
+  my $lock = ICG::Locker::Lock->new({
+    locker    => $self,
+    lock_id   => $dbh->last_insert_id,
+    expires   => $expires,
+    locked_by => $locked_by,
+  });
+
+  return $lock;
 }
 
 =head1 COPYRIGHT AND LICENSE
---
+++ lib/ICG/Locker/Lock.pm
@@ -0,0 +1,97 @@
+use strict;
+use warnings;
+use 5.008;
+
+package ICG::Locker::Lock;
+our $VERSION = '0.001';
+
+=head1 NAME
+
+ICG::Locker::Lock - a live resource lock
+
+=head1 METHODS
+
+=head2 new
+
+B<Calling this method is a very, very stupid idea.>  This method is called by
+L<ICG::Locker> to create locks.  Since you are not a locker, you should not
+call this method.  Seriously.
+
+  my $locker = ICG::Locker::Lock->new(\%arg);
+
+This returns a new lock. 
+
+  locker    - the locker creating the lock
+  lock_id   - the id of the lock in the lock table
+  expires   - the time (in epoch seconds) at which the lock will expire
+  locked_by - a hashref of identifying information
+
+=cut
+
+sub new {
+  my ($class, $arg) = @_;
+
+  my $guts = {
+    locker    => $arg->{locker},
+    lock_id   => $arg->{lock_id},
+    expires   => $arg->{expires},
+    locked_by => $arg->{locked_by},
+  };
+
+  return bless $guts => $class;
+}
+
+=head2 locker
+
+=head2 lock_id
+
+=head2 expires
+
+=head2 locked_by
+
+These are accessors for data supplied to L</new>.
+
+=cut
+
+BEGIN {
+  for my $attr (qw(locker lock_id expires locked_by)) {
+    Sub::Install::install_sub({
+      code => sub { $_[0]->{$attr} },
+      as   => $attr,
+    });
+  }
+}
+
+=head2 unlock
+
+This method unlocks the lock, deleting the semaphor record.  This method is
+automatically called when locks are garbage collected.
+
+=cut
+
+sub unlock {
+  my ($self) = @_;
+
+  my $dbh   = $self->locker->dbh;
+  my $table = $self->locker->table;
+
+  my $rows = $dbh->do("DELETE FROM $table WHERE id=?", undef, $self->lock_id);
+
+  X->throw('error releasing lock') unless $rows == 1;
+}
+
+sub DESTROY {
+  my ($self) = @_;
+  return unless $self->locked_by->{pid} == $$;
+  $self->unlock;
+}
+
+=head1 COPYRIGHT AND LICENSE
+
+Proprietary.  Copyright 2008 IC Group
+
+All rights reserved.
+
+=cut
+
+1;
